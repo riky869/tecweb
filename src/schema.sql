87c5ad53479ffcb9f956ae16b5c3c3048fcfc33f
Loris Libralato <lorislibra21@gmail.com>
2024-10-30 09:44:25 +0100

    feat: initial db schema

---
+++ src/schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS review,
+movie,
+user;
+
+CREATE TABLE IF NOT EXISTS movie (
+    id INT PRIMARY KEY,
+    name TEXT,
+    description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS review (
+    id INT PRIMARY KEY,
+    title TEXT,
+    content TEXT,
+    user_id INT REFERENCES user(id),
+    movie_id INT REFERENCES movie(id)
+);
+
+CREATE TABLE IF NOT EXISTS user (
+    id INT PRIMARY KEY,
+    username TEXT,
+    password TEXT,
+    name TEXT,
+    lastname TEXT
+);
