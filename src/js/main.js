1737fa2ffa119640a2faa68e716074a6f2b63194
Matteo Campagnaro <91309766+MatteoCampa@users.noreply.github.com>
2025-01-18 19:23:32 +0100

    fix hamburger.js

--- src/js/main.js
+++ src/js/main.js
@@ -230,4 +230,22 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   });
+
+  const btnOpen = document.getElementById('btnOpen');
+  const menu = document.querySelector('#menu');
+  const bread = document.querySelector('#breadcrumb');
+
+  // Funzione per aprire il menu
+  var btnOpenState = 'false';
+  btnOpen.addEventListener('click', function () {
+    btnOpenState = btnOpenState === 'false' ? 'true' : 'false';
+    if (btnOpenState === 'false') {
+      menu.classList.remove('active');
+      bread.classList.remove('active');
+    } else {
+      menu.classList.add('active');
+      bread.classList.add('active');
+    }
+    btnOpen.setAttribute('aria-expanded', btnOpenState);
+  });
 });
